fcdd76621672b5f716d6dc5761da5ed751499f28
Andrianovi <106331579+Andrianovi@users.noreply.github.com>
2024-11-04 18:04:06 +0500

    Add files via upload

---
+++ js/ims.js
@@ -0,0 +1,9 @@
+console.log('dff');
+window.addEventListener('load',function(){
+let imagesHidden = document.querySelectorAll('.img-hidden');
+
+imagesHidden.forEach(i => {
+console.log('dfsff');
+	i.style.display = 'block';
+});
+});
