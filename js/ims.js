47bb859b84ac093228553a026c7f23b6da62deee
Andrianovi <106331579+Andrianovi@users.noreply.github.com>
2024-11-05 17:24:02 +0500

    Delete js directory

--- js/ims.js
+++
@@ -1,9 +0,0 @@
-console.log('dff');
-window.addEventListener('load',function(){
-let imagesHidden = document.querySelectorAll('.img-hidden');
-
-imagesHidden.forEach(i => {
-console.log('dfsff');
-	i.style.display = 'block';
-});
-});
